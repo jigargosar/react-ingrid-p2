edae4c1a893b389ef677d9a6dddfbe1f6f8328c3
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-17 16:18:16 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { observer } from 'mobx-react-lite'
 
-function App() {
+const App = observer(() => {
   return <div className={`min-vh-100`}>HW</div>
-}
+})
+
+App.displayName = 'App'
 
 export default App
